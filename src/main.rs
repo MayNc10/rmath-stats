763c31b0e2e5fe908a6fd13344c24aa9417ec852
May <MayflowerC10@gmail.com>
2024-04-09 16:44:22 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,22 @@ use rand::prelude::*;
 use rayon::prelude::*;
 use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
+use coz::*;
 
 fn median_n(n: u32, rng: &mut SmallRng) -> f64 {
     let mut nums: Vec<f64>  = vec![rng.gen(); n as usize];
-    nums.par_sort_by(|a, b| a.partial_cmp(b).unwrap());
+    nums.par_sort_by(|a, b| { coz::thread_init(); a.partial_cmp(b).unwrap()} );
+    coz::scope!("median_n");
     nums[(n / 2) as usize]
 }
 
-const POWER: u32 = 8;
+const POWER: u32 = 9;
 const ITERATIONS: u64 = 10_u64.pow(POWER);
 const BATCH_SIZE: u64 = 10_u64.pow(3);
 const NUM_NS: u32 = 10;
 
 fn main() {
+    coz::thread_init();
     let mut thread_rng = thread_rng();
     let mut batch_vec = vec![0.0; BATCH_SIZE as usize];
     let mut small_rngs: Vec<SmallRng> = (0..BATCH_SIZE)
@@ -24,9 +27,10 @@ fn main() {
 
     let ns = (0..NUM_NS).map(|n| n * 2 + 1).collect::<Vec<_>>();
     for n in ns {
-        
+        coz::begin!("loop");
         let medians = small_rngs.par_iter_mut() 
             .map(|rng| {
+                coz::thread_init();
                 (0..(ITERATIONS / BATCH_SIZE)).into_iter()
                     .map(|_| median_n(n, rng))
                     .collect::<Vec<_>>()
@@ -41,8 +45,9 @@ fn main() {
             medians.append(&mut batch_vec);     
         }
         */
-
+        coz::end!("loop");
         let expected = medians.into_iter().sum::<f64>() / ITERATIONS as f64;
         println!("Determined expected median with n={n}, batch size {BATCH_SIZE} in {ITERATIONS} iterations. E = {expected}");
     }
+    coz::scope!("main");
 }
